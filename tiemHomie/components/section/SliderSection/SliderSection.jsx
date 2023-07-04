b98b3d527fbb6a2057b290652529f4b05a467f07
HoanFPTU <nguyenhoan20032003@gmail.com>
2023-07-04 17:56:16 +0700

    done

--- tiemHomie/components/section/SliderSection/SliderSection.jsx
+++ tiemHomie/components/section/SliderSection/SliderSection.jsx
@@ -6,38 +6,38 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "../../../styles/Home.module.css";
 const SliderSection = ({ sliderRef, products, showProductActionBox }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    loop: true,
+    arrows: false,
+    speed: 250,
+    slidesToShow: 4,
+    slidesToScroll: 1,
 
-    const settings = {
-        dots: false,
-        infinite: true,
-        loop: true,
-        arrows: false,
-        speed: 250,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-    
-        responsive: [
-          {
-            breakpoint: 800,
-            settings: {
-              slidesToShow: 2,
-              arrows: false,
-            },
-          },
-          {
-            breakpoint: 576,
-            settings: {
-              slidesToShow: 1,
-              arrows: false,
-            },
-          },
-        ],
-      };
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+        },
+      },
+    ],
+  };
 
-    return (
-      <div className={styles.shop} style={{ position: "relative" }}>
-        <Slider ref={sliderRef} {...settings}>
-          {products.length > 0 && products.map((product) => (
+  return (
+    <div className={styles.shop} style={{ position: "relative" }}>
+      <Slider ref={sliderRef} {...settings}>
+        {products.length > 0 &&
+          products.map((product) => (
             <div key={product.id} className="item">
               <ProductCard
                 productData={product}
@@ -45,10 +45,10 @@ const SliderSection = ({ sliderRef, products, showProductActionBox }) => {
               />
             </div>
           ))}
-        </Slider>
-        <Arrows sliderRef={sliderRef} />
-      </div>
-    );
-  };
+      </Slider>
+      <Arrows sliderRef={sliderRef} />
+    </div>
+  );
+};
 
 export default SliderSection;
